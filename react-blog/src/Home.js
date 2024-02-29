0bc29b2fafc9085b9473c99b3c0cb1ad7ce3fa74
Jordyn Lally <codingwithjordyn@gmail.com>
2024-02-29 17:48:41 -0500

    added custom fetch hook, basic routing

--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -1,24 +1,14 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-          return res.json();
-        })
-        .then(data => {
-          setBlogs(data);
-          setIsLoading(false);
-        })
-    }, []);
+    // fetches data from JSON server, run 'npx json-server --watch data/db.json --port 8000' to start server
+    const { data: blogs, isLoading, error } = useFetch('http://localhost:8000/blogs');
 
   return (
     <div className="home">
+      { error && <div>{error}</div> }
       { isLoading && <div>Loading...</div> }
       { blogs && <BlogList blogs={blogs}/> }
       { blogs && <BlogList blogs={blogs.filter(blog => blog.author === 'mario')} title="Mario's Blogs"/> }
